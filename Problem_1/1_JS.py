024eed3b8e26ec7cf9ef25f51ff3ccd22e017731
JuanseSalcedoG <jussalcedoga@unal.edu.co>
2018-12-14 12:22:07 -0500

    solving problem 1 in python

---
+++ Problem_1/1_JS.py
@@ -0,0 +1,13 @@
+def main():
+
+    numbers = 0
+    
+    for i in range(1000):
+
+        if (i % 3 == 0) or (i % 5 == 0): 
+            numbers += i
+
+    print (numbers)
+
+if __name__ == '__main__':
+    main()
