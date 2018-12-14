d13a562818a043745463163705141f125a76525c
jdalzatec <jdalzatec@unal.edu.co>
2018-12-13 23:17:22 -0500

    Solving the problem 4 in javascript

---
+++ Problem_4/main.js
@@ -0,0 +1,22 @@
+function isPalindrome(number)
+{
+    let reversed = number.toString().split("").reverse().join("");
+    return number == reversed;
+}
+
+
+let digits = 3;
+palindromes_arr = [];
+for (let i = Math.pow(10, digits - 1); i < Math.pow(10, digits); i++)
+{
+    for (let j = Math.pow(10, digits - 1); j < Math.pow(10, digits); j++)
+    {
+        let prod = i * j;
+        if (isPalindrome(prod))
+        {
+            palindromes_arr.push(prod);
+        }
+    }
+}
+
+console.log(Math.max(...palindromes_arr));
