c80f75914c0ef128f3d096d71a28492e5a3d8ee1
jdalzatec <jdalzatec@unal.edu.co>
2018-12-13 23:25:35 -0500

    A little faster

--- Problem_4/main.js
+++ Problem_4/main.js
@@ -9,7 +9,7 @@ let digits = 3;
 palindromes_arr = [];
 for (let i = Math.pow(10, digits - 1); i < Math.pow(10, digits); i++)
 {
-    for (let j = Math.pow(10, digits - 1); j < Math.pow(10, digits); j++)
+    for (let j = i; j < Math.pow(10, digits); j++)
     {
         let prod = i * j;
         if (isPalindrome(prod))
