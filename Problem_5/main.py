ab83378ebb26d716a76e7ad213765661bf547d8c
JuanseSalcedoG <jussalcedoga@unal.edu.co>
2018-12-17 15:56:22 -0500

    Solving Problem 5 in Python

--- Problem_5/main.py
+++ Problem_5/main.py
@@ -2,12 +2,11 @@ import numpy
 from collections import defaultdict
 
 def main(): 
-
-    # numbers = numpy.linspace(1, 20, 20, dtype = int)
-    numbers = numpy.linspace(1, 10, 10, dtype = int)
+    numbers = numpy.linspace(1, 20, 20, dtype = int)
     is_zero = defaultdict(list)
-    n = 1
+    n = 20
     while (n >= 1):
+        print(n)
         for i in numbers: 
             if n % i == 0:
                 is_zero[n].append(True)
@@ -15,7 +14,7 @@ def main():
                 is_zero[n].append(False)
         if (all(item == True for item in is_zero[n])):
             break
-        n += 1 
+        n += 20 
     print (n)
 if __name__ == '__main__':
     main()
