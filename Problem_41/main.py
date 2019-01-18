08cd853e0e224febde8375d3a8cc5b5972672e46
Daniel Felipe Rodríguez Patiño <dfrodriguezp@unal.edu.co>
2019-01-18 17:03:41 -0500

    Minor improvements in Python version of problem 41

--- Problem_41/main.py
+++ Problem_41/main.py
@@ -1,37 +1,30 @@
 from collections import Counter
 
 def RepeatingDigits(num):
-
     c = Counter(str(num))
     if any(value > 1 for value in c.values()):
         return True
-    else:
-        return False
+
+    return False
 
 def IsPandigital(num):
-
     digits = len(str(num))
     c = Counter(str(num))
     if not RepeatingDigits(num):
-        j = all(str(i) in c.keys() for i in range(1, digits + 1))
-        return j
+        return all(str(i) in c.keys() for i in range(1, digits + 1))
             
 def IsPrime(num): 
-
     if num > 1: 
-        for i in range(2, num//2): 
+        if (num != 2 and num % 2 == 0):
+            return False
+        for i in range(3, num//3 + 1, 2): 
             if (num % i) == 0: 
                 return False
-                break
-        else:
-            return True 
-    else: 
-        return False
+        return True 
+    return False
 
 def main():
-
-    largest = 7654321
-    n = largest
+    n = 7654321
     while n >= 1:
         if IsPandigital(n) and IsPrime(n):
             print (n)
